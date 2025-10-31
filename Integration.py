4553d1d79206b8487e8293aeccb9b2ddbd750531
weng <qwe11222017@gmail.com>
2025-10-31 11:54:27 +0800

    modifying integration.py

--- Integration.py
+++ Integration.py
@@ -68,7 +68,7 @@ def delete_student():
     student_id = entry_id.get()
     cursor.execute('SELECT * from DB_student where db_student_id = ?',(student_id,))
     delete = cursor.fetchall()
-    cursor.execute('DELETE * from DB_student where db_student_id = ?',(student_id))
+    cursor.execute('DELETE from DB_student where db_student_id = ?',(student_id))
     print('Following row are delete:', delete)
     conn.commit()
 
